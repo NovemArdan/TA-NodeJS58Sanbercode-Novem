3c7436d7fdbe93e34d3fb9938d39618a7dc0d46a
NovemArdan <novemrohmadin@gmail.com>
2024-08-09 11:11:29 +0700

    Tugas Akhir Tambah Fitur 5

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,30 @@
 // database.ts
+// import mongoose from 'mongoose';
+
+// const DATABASE_URL = "mongodb+srv://novemrohmadin4:novemrohmadin4@belajar-backend-nodejs.rc0fnwi.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejs";
+
+// const connect = async () => {
+//     try {
+//       await mongoose.connect(DATABASE_URL);
+//       console.log("Database connected");
+//     } catch (error) {
+//       console.error("Error connecting to database:", error);
+//     }
+// };
+
+// export default connect;
+
 import mongoose from 'mongoose';
 
 const DATABASE_URL = "mongodb+srv://novemrohmadin4:novemrohmadin4@belajar-backend-nodejs.rc0fnwi.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejs";
 
 const connect = async () => {
     try {
-      await mongoose.connect(DATABASE_URL);
+      await mongoose.connect(DATABASE_URL, {
+          autoIndex: true,
+          dbName: "test",  // Replace "nama db" with the actual name of your database
+          connectTimeoutMS: 10000
+      });
       console.log("Database connected");
     } catch (error) {
       console.error("Error connecting to database:", error);
@@ -13,3 +32,4 @@ const connect = async () => {
 };
 
 export default connect;
+
